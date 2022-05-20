700047703ef96363fc0c426fdc19353c517d8232
Joshua Williams <joshua.jwilliams@gmail.com>
2022-05-20 12:47:52 -0400

    adjust menu to allow for flexibility and initial protection

--- src/Accordian.js
+++ src/Accordian.js
@@ -1,18 +1,31 @@
-import { Accordion, AccordionPanel, Box, Text } from "grommet";
+import { Accordion, AccordionPanel, Box, Anchor } from "grommet";
+import { Link } from "react-router-dom";
+import { useAuth } from "./utils/auth";
 
 const MyAccordion = (props) => {
+  const auth = useAuth();
+
   //Get Inner Menu Items.
   const myInnerMenu = props.innerMenu.map((item) => {
     return (
-      <Box pad="medium" background="light-2">
-        <Text>{item}</Text>
+      <Box key={item.key} pad="medium" background="light-2">
+        <Anchor to={item.link} label={item.value} />
+        <Link to={item.link}>{item.value}</Link>
       </Box>
     );
   });
 
   return (
     <Accordion>
-      <AccordionPanel label={props.labelName}>{myInnerMenu}</AccordionPanel>
+      <AccordionPanel key={myInnerMenu} label={props.labelName}>
+        {myInnerMenu}
+      </AccordionPanel>
+
+      <AccordionPanel label="Account Info">
+        {!auth.user && <Link to="login">Login</Link>}
+
+        <Link to="protected">My Protected Page</Link>
+      </AccordionPanel>
     </Accordion>
   );
 };
