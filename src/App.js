b93754e9774c7e9f727156f21f5aa724cac4b84f
Joshua Williams <joshua.jwilliams@gmail.com>
2023-05-30 17:26:17 -0400

    basic layout system

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
-}
+//import { Layout, Header, Sider, Content, Footer } from "antd";
 
+// const App = () => {
+// };
+
+import { Layout, Space } from "antd";
+const { Header, Footer, Sider, Content } = Layout;
+const headerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 50,
+  lineHeight: "64px",
+  backgroundColor: "#7dbcea",
+};
+const contentStyle = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#108ee9",
+};
+const siderStyle = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#3ba0e9",
+};
+const footerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#7dbcea",
+};
+const App = () => (
+  <Space
+    direction="vertical"
+    style={{
+      width: "100%",
+    }}
+    size={[0, 48]}
+  >
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Sider style={siderStyle}>Sider</Sider>
+        <Content style={contentStyle}>Content</Content>
+      </Layout>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+  </Space>
+);
 export default App;
