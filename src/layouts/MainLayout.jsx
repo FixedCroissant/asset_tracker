4d09ae60992e504b914f73828605e149a73fa5e1
Joshua Williams <joshua.jwilliams@gmail.com>
2023-06-12 16:06:59 -0400

    updated text on layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -85,6 +85,7 @@ const MainLayout = () => {
               backgroundColor: "#fffff",
             }}
           >
+            Updated Text
             <Outlet />
           </Content>
         </Layout>
